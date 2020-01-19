b50b57d519fba344721fd78913a74a317f40fbe6
jakubtomal <jakub.tomal@smcebi.edu.pl>
2020-01-19 21:02:35 +0100

    Clean up

---
+++ TheGamexD/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private GameObject inventory;
+    [SerializeField] private GameObject slotHolder;
+    private bool _inventoryEnable;
+    private bool InventoryEnable
+    {
+        get
+        {
+            return _inventoryEnable;
+        }
+        set
+        {
+            _inventoryEnable = value;
+            inventory.SetActive(value);
+            
+        }
+    }
+
+    private int slotCount;
+    private int enabledSlotCount;
+    private GameObject[] slots;
+
+    private void Start()
+    {
+        InventoryEnable = false;
+        slotCount = slotHolder.transform.childCount;
+        slots = new GameObject[slotCount];
+        for(int i = 0; i < slotCount; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            InventoryEnable = !InventoryEnable;
+        }
+    }
+
+}
