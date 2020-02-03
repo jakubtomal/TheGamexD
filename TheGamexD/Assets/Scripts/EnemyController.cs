40d51daf6e55daa47a7eba2549f51ce849793f41
jakubtomal <jakub.tomal@smcebi.edu.pl>
2020-02-03 22:56:00 +0100

    Add EnemyController Script

---
+++ TheGamexD/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private float Starthealth = 100;
+    private float health;
+    private Animator myAnimtor;
+    private WaitForSeconds dieDelay = new WaitForSeconds(3f);
+    //Animator
+    private string triggerDead = "IsDead";
+    private string runningBool = "Running";
+    private string attackingBool = "Attacking";
+    void Start()
+    {
+        health = Starthealth;
+        myAnimtor = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0 )
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    private IEnumerator Die()
+    {
+        myAnimtor.SetTrigger("IsDead");
+        yield return dieDelay;
+        Destroy(gameObject);
+    }
+}
