5bd3773555524b4563c341395c0a54e4f2493ff9
jakubtomal <jakub.tomal@smcebi.edu.pl>
2020-02-03 22:05:07 +0100

    Add continuous shooting when fire key is pressing

--- TheGamexD/Assets/Scripts/ShootingController.cs
+++ TheGamexD/Assets/Scripts/ShootingController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class ShootingController : MonoBehaviour
 {
-    [SerializeField] GameObject projectile;
+    [SerializeField]
+    GameObject projectile;
+    [SerializeField]
+    float speed;
+    private bool isShooting = false;
+
     void Start()
     {
         
@@ -13,25 +18,24 @@ public class ShootingController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && !isShooting)
         {
-            Shoot();
-            //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            //Debug.Log(Camera.main.ScreenPointToRay(Input.mousePosition));
-            
+            StartCoroutine(Shoot());
         }
     }
 
-    private void Shoot()
+    private IEnumerator Shoot()
     {
+        isShooting = true;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
             Projectile currentProjectile = Instantiate(projectile, transform.position, transform.rotation).GetComponent<Projectile>();
-            Debug.Log(hit.transform);
             currentProjectile.Shoot(hit.point);
         }
+        yield return new WaitForSeconds(1.0f / speed);
+        isShooting = false;
 
     }
 }
