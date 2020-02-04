eefef2acdec6dfbb9a7866657a6234a1f8176bfe
Dziukens <35671349+Dziukens@users.noreply.github.com>
2020-02-04 19:27:10 +0100

    added new scenes (main menu, options)

---
+++ TheGamexD/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,43 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadStartScene()
+    {
+        Debug.Log("ELO, WITAJ W GIERCE!");
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadGameScene()
+    {
+        Debug.Log("JAZDA!");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadOptionsScene()
+    {
+        Debug.Log("USTAWIENIA!");
+        SceneManager.LoadScene("Options");
+    }
+
+    public void LoadNext()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void LoadGameOverScene()
+    {
+        Debug.Log("GG WP! YOU LOST!");
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("EXIT!");
+        Application.Quit();
+    }
+
+}
