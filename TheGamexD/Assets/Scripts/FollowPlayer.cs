1c8b2f8403ee041afc29c6aabcd85fd24582c403
jakubtomal <jakub.tomal@smcebi.edu.pl>
2020-02-04 21:47:28 +0100

    Add new camera in scene and script that allow camera follow player

---
+++ TheGamexD/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Player player;
+    [SerializeField]
+    private float yOffset;
+    [SerializeField]
+    private float zOffset;
+    [SerializeField]
+    private float cameraSpeed;
+    private Vector3 newCameraPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        transform.position = CalculateCameraPosition();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,CalculateCameraPosition(),cameraSpeed);
+    }
+
+    private Vector3 CalculateCameraPosition()
+    {
+        newCameraPosition = player.transform.position;
+        newCameraPosition.z -= zOffset;
+        newCameraPosition.y += yOffset;
+        return newCameraPosition;
+    }
+}
