3279ab2ffa4b6efe46f15a29b7d28bee640090bc
jakubtomal <jakub.tomal@smcebi.edu.pl>
2020-02-04 01:24:48 +0100

    Add remover script and attach to spell projectile

---
+++ TheGamexD/Assets/Scripts/Remover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Remover : MonoBehaviour
+{
+    [SerializeField]
+    private float delay;
+    private void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(Remove());
+    }
+
+    private IEnumerator Remove()
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+}
